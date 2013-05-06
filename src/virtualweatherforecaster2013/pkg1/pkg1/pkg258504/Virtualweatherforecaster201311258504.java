aa7f4a0444709a68c32f844463d6f3093daff931
gmcabanah@unal.edu.co <gmcabanah@unal.edu.co>
2013-05-05 22:34:08 -0500

    Init Commit

---
+++ src/virtualweatherforecaster2013/pkg1/pkg1/pkg258504/Virtualweatherforecaster201311258504.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package virtualweatherforecaster2013.pkg1.pkg1.pkg258504;
+
+/**
+ *
+ * @author JULIAN
+ */
+public class Virtualweatherforecaster201311258504 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
