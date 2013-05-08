325805d10247de09434d2c5cc3290ba8eaf4a009
Santiniet <Santiniet@Santiniet-HP>
2013-05-08 01:53:41 -0500

    Winters Holt aplicado

--- src/Algorithms/HoltWinters.java
+++ src/Algorithms/HoltWinters.java
@@ -6,8 +6,28 @@ package Algorithms;
 
 /**
  *
- * @author Daniel
+ * @author Santiniet
  */
 public class HoltWinters {
+    
+    public double[] holtwinter(double[] data, double alpha, double beta){
+        int t= data.length;
+        double[] A= null;
+        double[] B= null;
+        double[] F= null;
+        //Se inicializan los arreglos para operaciones
+        A[0]=data[0];
+        B[0]= data[1]-data[0];
+        //Se inicializan los primeros datos del arreglo
+        for (int i=1; i<t; i++){
+            A[i]= (alpha*data[i])+((1-alpha)*(A[i-1]+B[i-1]));
+            B[i]= (beta*(A[i]-A[i-1]))+((1-beta)*B[i-1]);
+            F[i]= A[i]+B[i];
+        }
+        
+        
+        return F;
+    }
+    
     
 }
