5e54a1b6669bcb90d2769970ab12666b4bc57e9b
Pavat <Pavat@Pavat-PC>
2013-05-08 23:21:08 -0500

    Commit by: Pedro Valderrama Changes: Starter for MAPE in DES update

--- src/Algorithms/HoltWinters.java
+++ src/Algorithms/HoltWinters.java
@@ -15,30 +15,43 @@ public class HoltWinters {
     ArrayList<Double> data = new ArrayList<Double>();
     ArrayList<Double> A = new ArrayList<Double>();
     ArrayList<Double> B = new ArrayList<Double>();
+    ArrayList<Double> C= new ArrayList<Double>();
     ArrayList<Double> F = new ArrayList<Double>();
     
     
     final double alpha=0.5;
     final double beta=0.5;
-            
+    final double gamma=0.5;
+    
     public HoltWinters(ArrayList<Double> data) {
         this.data=data;
         }
     
-    public ArrayList<Double> holtwinter(ArrayList<Double> data){
+    public ArrayList<Double> holtwinter(ArrayList<Double> data, int m){
         
         int t= data.size();
+        int l=2;
+        int N=t/l;
+        
+        double ci, aj;
         //Se inicializan los arreglos para operaciones
         A.add(data.get(0));
-        B.add(data.get(1)-data.get(0));
+        B.add((((data.get(l+1)-data.get(1))/l)+ ((data.get(l+2)-data.get(2))/l))/l);
+        for(int i = 1;i <= l; ++i) {
+            ci = 0;
+            for(int j = 1; j <= N; ++j) {
+                
+            }
+        }
+        
         //Se inicializan los primeros datos del arreglo
         for (int i=1; i<t; i++){
             //A[i]= (alpha*data.get(i))+((1-alpha)*(A[i-1]+B[i-1]));
             A.add(i, (alpha*data.get(i))+((1-alpha)*(A.get(i-1)+B.get(i-1))));
             //B[i]= (beta*(A[i]-A[i-1]))+((1-beta)*B[i-1]);
             B.add(i, (beta*(A.get(i)-A.get(i-1)))+((1-beta)*B.get(i-1)));
+            
             //F[i]= A[i]+B[i];
-            F.add(A.get(i)+B.get(i));
         }
         
         
--- src/Algorithms/MAPE.java
+++ src/Algorithms/MAPE.java
@@ -4,10 +4,24 @@
  */
 package Algorithms;
 
+import java.util.ArrayList;
+
 /**
  *
- * @author Daniel
+ * @author Santiniet
  */
 public class MAPE {
+    
+    public double calculate(ArrayList<Double> yt, ArrayList<Double> ft) {
+        double m = 0.0;
+        int n = Math.min(yt.size(), ft.size());
+        
+        for(int i = 0; i < n; ++i) {
+            m += Math.abs((yt.get(i) - ft.get(i)) / yt.get(i));
+        }
+        
+        m /= (double) n;
+        return m;
+    }
     
 }
